74547945fc7be47fa68a2415330f7a4dbc01fe1d
Adham <Adham-461@hotmail.com>
2023-11-09 14:12:25 +0300

    Adding Client Reviews for Experience Page

--- src/app/experience/page.jsx
+++ src/app/experience/page.jsx
@@ -2,14 +2,20 @@ import styles from "./page.module.css";
 import experienceData from "@/Data/experienceData.json";
 export const metadata = {
   title: `Adham's Work Experience`,
-}
+};
 const Experience = () => {
   return (
     <section className={styles.experience}>
-      <h2 className={styles.experiencePageTitle} data-aos="fade-down">Work Experience</h2>
+      <h2 className={styles.experiencePageTitle} data-aos="fade-down">
+        Work Experience
+      </h2>
       <section className={styles.experienceSection}>
         {experienceData.map((experience) => (
-          <div className={styles.experienceCard} key={experience.id} data-aos="fade-up">
+          <div
+            className={styles.experienceCard}
+            key={experience.id}
+            data-aos="fade-up"
+          >
             <div className={styles.experienceDate}>
               <span>{experience.date}</span>
             </div>
@@ -23,6 +29,29 @@ const Experience = () => {
                   <li key={index}>{item}</li>
                 ))}
               </ul>
+              {experience.reviews && (
+                <div className={styles.reviews}>
+                  <h4>Client Reviews:</h4>
+                  <ul>
+                    {experience.reviews.map((review, index) => (
+                      <li key={index}>
+                        {Object.entries(review).map(([platform, link]) => (
+                          <p key={platform}>
+                            {platform}:{" "}
+                            <a
+                              href={link}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              {link}
+                            </a>
+                          </p>
+                        ))}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
             </div>
           </div>
         ))}
